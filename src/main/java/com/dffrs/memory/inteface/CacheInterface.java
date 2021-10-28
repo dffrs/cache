7050fb29fb4ee9ab59d69d321924233fc160a4c9
dffrs <dffrs@iscte-iul.pt>
2021-10-28 14:09:58 +0100

    Changed a return value for get() method.

--- src/main/java/com/dffrs/memory/inteface/CacheInterface.java
+++ src/main/java/com/dffrs/memory/inteface/CacheInterface.java
@@ -4,7 +4,7 @@ package com.dffrs.memory.inteface;
  * Interface to accomudate default cache operations.
  */
 public interface CacheInterface<K, V> {
-    public K get(K key);
+    public V get(K key);
     public void addElement(K key, V element);
     public void removeElement(K key);
     public int getSize();
